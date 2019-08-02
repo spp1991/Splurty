f7be457678962e797e030323922e4c4fd90b34ae
Sunny Patel <spp1991@gmail.com>
2019-08-02 03:16:09 +0000

    Added validation and database addition code

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates:saying , presence:true , length:{ minimum: 3, maximum: 140 }
+  validates:author , presence:true , length:{ minimum: 3, maximum: 50 }
 end
